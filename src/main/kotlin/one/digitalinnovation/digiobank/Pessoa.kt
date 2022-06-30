b277250dcd1345e27f04dab9e950fbbd06cc8a38
Thayllan <thayllanh@hotmail.com>
2022-06-30 14:46:38 -0300

    Criação da classe Pessoa.kt

--- src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
@@ -1,4 +1,12 @@
 package one.digitalinnovation.digiobank
 
 class Pessoa {
+    var nome: String = "Jether"
+    var cpf: String = "123.123.123-11"
+}
+
+fun main(){
+    val jether = Pessoa()
+    println(jether.nome)
+    println(jether.cpf)
 }
