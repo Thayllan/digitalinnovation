c7e8f902e8d5f6684fbf8eda729a999a42a2abb4
Thayllan <thayllanh@hotmail.com>
2022-07-05 16:36:24 -0300

    apply private set

--- src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digiobank/Pessoa.kt
@@ -3,10 +3,18 @@ package one.digitalinnovation.digiobank
 class Pessoa {
     var nome: String = "Jether"
     var cpf: String = "123.123.123-11"
+    private set
+
 }
+
+
 
 fun main(){
     val jether = Pessoa()
+    println(jether)
     println(jether.nome)
     println(jether.cpf)
+
+
+
 }
